90a533244f6002623000ec20739422ef3e4bff1f
vanchistelroy <vanchistelroy@gmail.com>
2026-07-22 18:16:53 -0500

    Fix website startup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactPixel from 'react-facebook-pixel'
 import './index.css'
 import App from './App.jsx'
-
-const PIXEL_ID = '1057835640078985'
-
-ReactPixel.init(PIXEL_ID)
-ReactPixel.pageView()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
